67b0cd6d5865bfd69018cb6002523ef29278134d
OneFool <111654008+OneFool@users.noreply.github.com>
2023-06-04 15:19:38 -0500

    Update SpotifyFixer.bat

--- SpotifyFixer.bat
+++ SpotifyFixer.bat
@@ -1,4 +1,5 @@
 :: SpotifyFixer by OneFool#7876
+:: Use this if your spotify bugs out and you can't pause/play music etc.
 taskkill /f /im Spotify.exe /T > nul
 timeout /t 1 /nobreak
 :: Replace the example Spotify.exe path below with your path.
