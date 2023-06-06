6198bcf940337777ce5af2c49f11eec41f185551
OneFool <111654008+OneFool@users.noreply.github.com>
2023-06-06 13:02:47 -0500

    Remove useless timeout + more commenting

--- SpotifyFixer.bat
+++ SpotifyFixer.bat
@@ -1,6 +1,7 @@
 :: SpotifyFixer by OneFool#7876
 :: Use this if your spotify bugs out and you can't pause/play music etc.
-taskkill /f /im Spotify.exe /T > nul
-timeout /t 1 /nobreak
-:: Replace the example Spotify.exe path below with your path.
-START C:\Users\OneFool\AppData\Roaming\Spotify\Spotify.exe
+:: This will close out all instances of spotify which task manager usually doesn't do if Spotify is bugged resulting in;
+:: the "Spotify is not responding" message when you try to re-launch it.
+
+taskkill /f /im Spotify.exe /T > nul :: Close out Spotify
+START C:\Users\OneFool\AppData\Roaming\Spotify\Spotify.exe :: Re-Launch Spotify, make sure to replace the example path with your Spotify.exe path
