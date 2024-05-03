5cd7a9207e4dc9aaae7e8295a3c08baedb8b67ed
OneFool <111654008+OneFool@users.noreply.github.com>
2024-05-03 11:59:37 -0500

    Update Arcane Lineage Auto-Dupe.lua

--- Arcane Lineage Auto-Dupe.lua
+++ Arcane Lineage Auto-Dupe.lua
@@ -24,6 +24,8 @@ for _, v in next, getgc() do
 end
 -- End Adonis Bypasses
 
+task.wait(3)
+
 -- Client AntiKick
 OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
     if not checkcaller() and string.lower(getnamecallmethod()) == "kick" then
